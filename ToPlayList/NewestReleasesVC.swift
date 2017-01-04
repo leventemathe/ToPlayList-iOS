adbc8c28d1afbe3fe30a632a02b2ed30cdae21d0
Máthé Levente <mathelevente@Levis-MacBook-Air.local>
2017-01-04 23:34:57 +0100

    Error handling in newest releases list done.

--- ToPlayList/NewestReleasesVC.swift
+++ ToPlayList/NewestReleasesVC.swift
@@ -14,6 +14,10 @@ class NewestReleasesVC: UIViewController, UITableViewDelegate, UITableViewDataSo
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var navBar: ShadowyView!
     
+    private var noDataLbl = UILabel()
+    
+    private var refreshVC = UIRefreshControl()
+    
     private var games = [Game]() {
         didSet {
             tableView.reloadData()
@@ -26,19 +30,54 @@ class NewestReleasesVC: UIViewController, UITableViewDelegate, UITableViewDataSo
         tableView.dataSource = self
         navBar.addDropShadow()
         
+        noDataLbl.bounds = CGRect(x: 0.0, y: 0.0, width: tableView.bounds.width, height: tableView.bounds.height)
+        noDataLbl.text = "No data. Pull to refresh!"
+        noDataLbl.textAlignment = NSTextAlignment.center
+        noDataLbl.sizeToFit()
+        
+        refreshVC.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
+        tableView.addSubview(refreshVC)
+        
+        reloadGames()
+    }
+
+    @objc private func refresh(_ sender: AnyObject) {
+        reloadGames()
+    }
+    
+    private func reloadGames() {
         IGDB.instance.getNewestGamesList { result in
             switch result {
             case .succes(let games):
-                self.games.append(contentsOf: games)
+                self.games = games
+                self.resetListBackground()
             case .failure(let error):
+                self.setListBackground()
                 switch error {
                 case .serverError, .urlError, .jsonError:
                     self.presentNetworkingError(with: "There was an error with the server, please check back later!")
+                    break
                 case .noInternetError:
                     self.presentNetworkingError(with: "No internet connection!")
+                    break
                 }
             }
+            self.refreshVC.endRefreshing()
         }
+    }
+
+    //TODO move to TableViewDelegates with delegate methods
+    private func setListBackground() {
+        if(games.count > 0) {
+            return
+        }
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.none
+        tableView.backgroundView = noDataLbl
+    }
+    
+    private func resetListBackground() {
+        tableView.backgroundView = nil
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.singleLine
     }
 
     private func presentNetworkingError(with message: String) {
@@ -65,4 +104,9 @@ class NewestReleasesVC: UIViewController, UITableViewDelegate, UITableViewDataSo
         return games.count
     }
 }
+
+
+
+
+
 
