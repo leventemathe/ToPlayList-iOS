79939c146ee4a84bad710fd50d29aaee76f70ffd
Levente Máthé <leviouss@gmail.com>
2017-04-10 11:35:06 +0200

    Integrated new both-lists listener system in newest releases vc.

--- ToPlayList/NewestReleasesVC.swift
+++ ToPlayList/NewestReleasesVC.swift
@@ -33,15 +33,7 @@ class NewestReleasesVC: UIViewController, UITableViewDelegate, UITableViewDataSo
     private let paginationLimit = 10
     private var paginationOffset = 0
     
-    private var toPlayListListenerAdd: ListsListenerReference?
-    private var playedListListenerAdd: ListsListenerReference?
-    private var toPlayListListenerRemove: ListsListenerReference?
-    private var playedListListenerRemove: ListsListenerReference?
-    
-    private var shouldRemoveToPlayListListenerAdd = 0
-    private var shouldRemoveToPlayListListenerRemove = 0
-    private var shouldRemovePlayedListListenerAdd = 0
-    private var shouldRemovePlayedListListenerRemove = 0
+    private var listsListenerSystem = ToPlayAndPlayedListListeners()
     
     private var toPlayList = List(ListsEndpoints.List.TO_PLAY_LIST)
     private var playedList = List(ListsEndpoints.List.PLAYED_LIST)
@@ -91,6 +83,7 @@ class NewestReleasesVC: UIViewController, UITableViewDelegate, UITableViewDataSo
         clearStars()
         getGamesInLists {
             self.tableView.reloadData()
+            self.listsListenerSystem.errorHandlerDelegate = self
             self.attachListListeners()
         }
     }
@@ -102,7 +95,7 @@ class NewestReleasesVC: UIViewController, UITableViewDelegate, UITableViewDataSo
     }
     
     override func viewDidDisappear(_ animated: Bool) {
-        removeListListeners()
+        self.listsListenerSystem.detachListeners()
     }
     
     private func getGamesInLists(_ onComplete: @escaping ()->()) {
@@ -126,164 +119,25 @@ class NewestReleasesVC: UIViewController, UITableViewDelegate, UITableViewDataSo
         if !ListsUser.loggedIn {
             return
         }
-        listenToToPlayListAdd()
-        listenToPlayedListAdd()
-        listenToToPlayListRemove()
-        listenToPlayedListRemove()
-    }
-    
-    private func removeListListeners() {
-        removeToPlayListListenerAdd()
-        removePlayedListListenerAdd()
-        removeToPlayListListenerRemove()
-        removePlayedListListenerRemove()
-    }
-    
-    func removeToPlayListListenerAdd() {
-        if toPlayListListenerAdd != nil {
-            toPlayListListenerAdd!.removeListener()
-            toPlayListListenerAdd = nil
-        } else {
-            shouldRemoveToPlayListListenerAdd += 1
-        }
-    }
-    
-    func removePlayedListListenerAdd() {
-        if playedListListenerAdd != nil {
-            playedListListenerAdd!.removeListener()
-            playedListListenerAdd = nil
-        } else {
-            shouldRemovePlayedListListenerAdd += 1
-        }
-    }
-    
-    func removeToPlayListListenerRemove() {
-        if toPlayListListenerRemove != nil {
-            toPlayListListenerRemove!.removeListener()
-            toPlayListListenerRemove = nil
-        } else {
-            shouldRemoveToPlayListListenerRemove += 1
-        }
-    }
-    
-    func removePlayedListListenerRemove() {
-        if playedListListenerRemove != nil {
-            playedListListenerRemove!.removeListener()
-            playedListListenerRemove = nil
-        } else {
-            shouldRemovePlayedListListenerRemove += 1
-        }
-    }
-    
-    private func listenToToPlayListAdd() {
-        listenToList(ListsEndpoints.List.TO_PLAY_LIST, withAction: .add) { game in
+        listsListenerSystem.attachListeners(withOnAddedToToPlayList: { game in
             if self.toPlayList.add(game) {
                 self.tableView.reloadData()
             }
             print("set content newest releases")
-        }
-    }
-    
-    private func listenToPlayedListAdd() {
-        listenToList(ListsEndpoints.List.PLAYED_LIST, withAction: .add) { game in
+        }, withOnRemovedFromToPlayList: { game in
+            self.toPlayList.remove(game)
+            self.tableView.reloadData()
+            print("set content newest releases")
+        }, withOnAddedToPlayedList: { game in
             if self.playedList.add(game) {
                 self.tableView.reloadData()
             }
             print("set content newest releases")
-        }
-    }
-    
-    private func listenToToPlayListRemove() {
-        listenToList(ListsEndpoints.List.TO_PLAY_LIST, withAction: .remove) { game in
-            self.toPlayList.remove(game)
-            self.tableView.reloadData()
-            print("set content newest releases")
-        }
-    }
-    
-    private func listenToPlayedListRemove() {
-        listenToList(ListsEndpoints.List.PLAYED_LIST, withAction: .remove) { game in
+        }, withOnRemovedFromPlayedList: { game in
             self.playedList.remove(game)
             self.tableView.reloadData()
             print("set content newest releases")
-        }
-    }
-
-    private func listenToList(_ list: String, withAction action: ListsListenerAction, withOnChange onChange: @escaping (Game)->()) {
-        ListsList.instance.listenToList(list, withAction: action, withListenerAttached: { result in
-            switch result {
-            case .succes(let ref):
-                self.listListenerAttachmentSuccesful(list, withAction: action, forReference: ref)
-            case .failure(let error):
-                switch error {
-                default:
-                    Alerts.alertWithOKButton(withMessage: Alerts.UNKNOWN_LISTS_ERROR, forVC: self)
-                }
-            }
-        }, withOnChange: { result in
-            switch result {
-            case .succes(let game):
-                onChange(game)
-            case .failure(let error):
-                switch error {
-                default:
-                    Alerts.alertWithOKButton(withMessage: Alerts.UNKNOWN_LISTS_ERROR, forVC: self)
-                }
-            }
         })
-    }
-    
-    private func listListenerAttachmentSuccesful(_ list: String, withAction action: ListsListenerAction, forReference ref: ListsListenerReference) {
-        switch action {
-        case .add:
-            if list == ListsEndpoints.List.TO_PLAY_LIST {
-                self.toPlayListListenerAdd = ref
-                self.removeLateToPlayListListenerAdd()
-            } else if list == ListsEndpoints.List.PLAYED_LIST {
-                self.playedListListenerAdd = ref
-                self.removeLatePlayedListListenerAdd()
-            }
-        case .remove:
-            if list == ListsEndpoints.List.TO_PLAY_LIST {
-                self.toPlayListListenerRemove = ref
-                self.removeLateToPlayListListenerRemove()
-            } else if list == ListsEndpoints.List.PLAYED_LIST {
-                self.playedListListenerRemove = ref
-                self.removeLatePlayedListListenerRemove()
-            }
-        }
-    }
-    
-    private func removeLateToPlayListListenerAdd() {
-        if shouldRemoveToPlayListListenerAdd > 0 {
-            toPlayListListenerAdd!.removeListener()
-            toPlayListListenerAdd = nil
-            shouldRemoveToPlayListListenerAdd -= 1
-        }
-    }
-    
-    private func removeLatePlayedListListenerAdd() {
-        if shouldRemovePlayedListListenerAdd > 0 {
-            playedListListenerAdd!.removeListener()
-            playedListListenerAdd = nil
-            shouldRemovePlayedListListenerAdd -= 1
-        }
-    }
-    
-    private func removeLateToPlayListListenerRemove() {
-        if shouldRemoveToPlayListListenerRemove > 0 {
-            toPlayListListenerRemove!.removeListener()
-            toPlayListListenerRemove = nil
-            shouldRemoveToPlayListListenerRemove -= 1
-        }
-    }
-    
-    private func removeLatePlayedListListenerRemove() {
-        if shouldRemovePlayedListListenerRemove > 0 {
-            playedListListenerRemove!.removeListener()
-            playedListListenerRemove = nil
-            shouldRemovePlayedListListenerRemove -= 1
-        }
     }
     
     private func getStarState(_ game: Game) -> StarState {
