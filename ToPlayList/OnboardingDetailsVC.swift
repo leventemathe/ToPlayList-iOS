eac4ecf73de2f25a528fdfa3a535155447c4fb80
Levente Máthé <leviouss@gmail.com>
2017-09-06 14:01:32 +0200

    Added start button with animations.

--- ToPlayList/OnboardingDetailsVC.swift
+++ ToPlayList/OnboardingDetailsVC.swift
@@ -14,6 +14,8 @@ class OnboardingDetailsVC: UIViewController {
     @IBOutlet weak var iphoneImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var startButtonContainer: UIView!
+    @IBOutlet weak var startButtonContainerTopConstraint: NSLayoutConstraint!
     
     var lastVCInPageVC = false
     
@@ -36,5 +38,20 @@ class OnboardingDetailsVC: UIViewController {
         if shouldFlipBackground {
             backgroundImage.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
         }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if lastVCInPageVC && startButtonContainer.isHidden {
+            animateStartButtonAppearance()
+        }
+    }
+    
+    private func animateStartButtonAppearance() {
+        startButtonContainer.isHidden = false
+        startButtonContainerTopConstraint.constant = -400
+        UIView.animate(withDuration: 1.0, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.2, options: [], animations: {
+            self.startButtonContainerTopConstraint.constant = -40
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
 }
