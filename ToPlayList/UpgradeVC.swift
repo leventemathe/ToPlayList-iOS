bc93b015083f57568a21321a9e512c23b18a3708
Levente Máthé <leviouss@gmail.com>
2017-09-13 14:11:28 +0200

    Layout done for upgrade screen.

--- ToPlayList/UpgradeVC.swift
+++ ToPlayList/UpgradeVC.swift
@@ -13,28 +13,53 @@ class UpgradeVC: UIViewController {
     private let NETWORK_ERROR = "No internet connection."
     private let SERVER_ERROR = "There was an error on the server. Please try again later."
     
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var backgroundImageLeftConstraint: NSLayoutConstraint!
     @IBOutlet weak var upgradeButton: LoginSceneButtonLogin!
     
     @IBAction func upgradeButtonClicked(_ sender: UIButton) {
         
     }
     
-    private let api = InappPurchasesService.instance
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        api.getProductIDs { result in
-            switch result {
-            case .success(let ids):
-                print(ids)
-            case .failure(let error):
-                switch error {
-                case .network:
-                    Alerts.alertWithOKButton(withMessage: self.NETWORK_ERROR, forVC: self)
-                case .server:
-                    Alerts.alertWithOKButton(withMessage: self.SERVER_ERROR, forVC: self)
-                }
-            }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        backgroundImageLeftConstraint.constant = -60
+        addTiltingBackground()
+        upgradeButton.startLoadingAnimation()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        removeTiltingBackground()
+        backgroundImageLeftConstraint.constant = 0
+    }
+    
+    var horizontalMotion: UIInterpolatingMotionEffect?
+    var verticalMotion: UIInterpolatingMotionEffect?
+    
+    private func addTiltingBackground() {
+        let amount = 50
+        
+        if horizontalMotion == nil || verticalMotion == nil {
+            horizontalMotion = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
+            horizontalMotion!.minimumRelativeValue = -amount
+            horizontalMotion!.maximumRelativeValue = amount
+            
+            verticalMotion = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis)
+            verticalMotion!.minimumRelativeValue = -amount
+            verticalMotion!.maximumRelativeValue = amount
+        }
+        
+        backgroundImageView.addMotionEffect(horizontalMotion!)
+        backgroundImageView.addMotionEffect(verticalMotion!)
+    }
+    
+    private func removeTiltingBackground() {
+        if let hor = horizontalMotion, let ver = verticalMotion {
+            backgroundImageView.removeMotionEffect(hor)
+            backgroundImageView.removeMotionEffect(ver)
         }
     }
 }
